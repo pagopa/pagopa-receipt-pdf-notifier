2fc025c946fb4ee0bef26c94894c019d0db00c22
svariant <samuele.varianti@nttdata.com>
2023-06-21 12:22:33 +0200

    [PRDP-36] Added Receipt entity

---
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/entity/receipt/Receipt.java
@@ -0,0 +1,23 @@
+package it.gov.pagopa.receipt.pdf.notifier.entity.receipt;
+
+import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.enumeration.ReceiptStatusType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Receipt {
+
+    private String idEvent;
+    private String id;
+    private String version;
+    private EventData eventData;
+    private IOMessageData ioMessageData;
+    private ReceiptStatusType status;
+    private ReceiptMetadata mdAttach;
+    private ReceiptMetadata mdAttachPayer;
+    private int numRetry;
+    private ReasonError reasonErr;
+}
---
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/entity/receipt/enumeration/ReceiptStatusType.java
@@ -0,0 +1,5 @@
+package it.gov.pagopa.receipt.pdf.notifier.entity.receipt.enumeration;
+
+public enum ReceiptStatusType {
+    NOT_QUEUE_SENT, INSERTED, RETRY, GENERATED, SIGNED, FAILED, IO_NOTIFIED, IO_NOTIFIER_RETRY
+}
