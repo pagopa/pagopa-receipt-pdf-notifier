8ceb9c23a5dbaf1c97f83fc80b1f20d710d67b96
svariant <samuele.varianti@nttdata.com>
2023-06-27 17:31:07 +0200

    [PRDP-36] Added error handling for both API calls to IO services

--- src/main/java/it/gov/pagopa/receipt/pdf/notifier/ReceiptToIO.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/ReceiptToIO.java
@@ -6,17 +6,12 @@ import com.microsoft.azure.functions.annotation.CosmosDBOutput;
 import com.microsoft.azure.functions.annotation.CosmosDBTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.QueueOutput;
-import it.gov.pagopa.receipt.pdf.notifier.client.generated.ApiException;
-import it.gov.pagopa.receipt.pdf.notifier.client.generated.ApiResponse;
-import it.gov.pagopa.receipt.pdf.notifier.client.generated.api.IOClient;
-import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.IOMessageData;
+import it.gov.pagopa.receipt.pdf.notifier.entity.message.IOMessage;
 import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.enumeration.ReceiptStatusType;
-import it.gov.pagopa.receipt.pdf.notifier.exception.ErrorToNotifyException;
-import it.gov.pagopa.receipt.pdf.notifier.model.generated.CreatedMessage;
-import it.gov.pagopa.receipt.pdf.notifier.model.generated.FiscalCodePayload;
-import it.gov.pagopa.receipt.pdf.notifier.model.generated.LimitedProfile;
-import it.gov.pagopa.receipt.pdf.notifier.model.generated.NewMessage;
+import it.gov.pagopa.receipt.pdf.notifier.model.enumeration.UserNotifyStatus;
+import it.gov.pagopa.receipt.pdf.notifier.model.enumeration.UserType;
+import it.gov.pagopa.receipt.pdf.notifier.service.ReceiptToIOService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -29,9 +24,6 @@ import java.util.logging.Logger;
  * Azure Functions with CosmosDB trigger.
  */
 public class ReceiptToIO {
-
-    private final static String PAYER = "payer";
-    private final static String DEBTOR = "debtor";
 
     @FunctionName("ReceiptToIoProcessor")
     //TODO @ExponentialBackoffRetry(maxRetryCount = 5, minimumInterval = "500", maximumInterval = "5000")
@@ -52,7 +44,7 @@ public class ReceiptToIO {
                     //TODO change with correct queue name
                     queueName = "pagopa-d-weu-receipts-queue-receipt-waiting-4-gen",
                     connection = "NOTIFIER_QUEUE_CONN_STRING")
-            OutputBinding<String> requeueMessage,
+            OutputBinding<String> requeueMessages,
             @CosmosDBOutput(
                     name = "ReceiptDatastore",
                     databaseName = "db",
@@ -65,7 +57,7 @@ public class ReceiptToIO {
                     //TODO change with correct collection name
                     collectionName = "messages",
                     connectionStringSetting = "COSMOS_IO_MESSAGE_CONN_STRING")
-            OutputBinding<List<Map<String, String>>> documentMessages,
+            OutputBinding<List<IOMessage>> documentMessages,
             final ExecutionContext context
     ) {
         Logger logger = context.getLogger();
@@ -75,7 +67,7 @@ public class ReceiptToIO {
         int discarder = 0;
 
         List<Receipt> receiptsNotified = new ArrayList<>();
-        List<Map<String, String>> messagesNotified = new ArrayList<>();
+        List<IOMessage> messagesNotified = new ArrayList<>();
         int queueSent = 0;
 
         for (Receipt receipt : listReceipts) {
@@ -86,33 +78,35 @@ public class ReceiptToIO {
                             receipt.getStatus().equals(ReceiptStatusType.IO_NOTIFIER_RETRY))
             ) {
 
-                try {
-                    String debtorFiscalCode = receipt.getEventData().getDebtorFiscalCode();
-                    String payerFiscalCode = receipt.getEventData().getPayerFiscalCode();
 
-                    boolean notifyOnlyDebtor = payerFiscalCode == null || payerFiscalCode.equals(debtorFiscalCode);
+                String debtorFiscalCode = receipt.getEventData().getDebtorFiscalCode();
+                String payerFiscalCode = receipt.getEventData().getPayerFiscalCode();
 
-                    if (debtorFiscalCode != null &&
-                            (receipt.getIoMessageData() == null ||
-                                    receipt.getIoMessageData().getIdMessageDebtor() == null)
-                    ) {
-                        notifyMessage(debtorFiscalCode, DEBTOR);
-                    }
-                    if (payerFiscalCode != null &&
-                            (receipt.getIoMessageData() == null ||
-                                    receipt.getIoMessageData().getIdMessagePayer() == null
-                            )
-                    ) {
-                        notifyMessage(payerFiscalCode, PAYER);
-                    }
+                Map<String, UserNotifyStatus> usersToBeVerified = new HashMap<>();
 
-                    verifyMessagesNotification(messagesNotified, receipt, notifyOnlyDebtor);
+                ReceiptToIOService service = new ReceiptToIOService();
 
-                } catch (ErrorToNotifyException e) {
-                    receipt.setStatus(ReceiptStatusType.IO_NOTIFIER_RETRY);
-                    //TODO save on queue on error
-                    queueSent++;
+                if (debtorFiscalCode != null &&
+                        (receipt.getIoMessageData() == null ||
+                                receipt.getIoMessageData().getIdMessageDebtor() == null)
+                ) {
+                    service.notifyMessage(usersToBeVerified, debtorFiscalCode, UserType.DEBTOR, logger);
                 }
+                if (payerFiscalCode != null &&
+                        (receipt.getIoMessageData() == null ||
+                                receipt.getIoMessageData().getIdMessagePayer() == null
+                        )
+                ) {
+                    service.notifyMessage(usersToBeVerified, payerFiscalCode, UserType.PAYER, logger);
+                }
+
+                queueSent += service.verifyMessagesNotification(
+                        usersToBeVerified,
+                        messagesNotified,
+                        receipt,
+                        requeueMessages,
+                        logger
+                );
 
                 receiptsNotified.add(receipt);
             } else {
@@ -144,73 +138,5 @@ public class ReceiptToIO {
             documentMessages.setValue(messagesNotified);
         }
 
-    }
-
-    private static void verifyMessagesNotification(List<Map<String, String>> messagesNotified, Receipt receipt, boolean notifyOnlyDebtor) throws ErrorToNotifyException {
-        if (receipt.getIoMessageData() != null) {
-            Map<String, String> debtorMessageData = new HashMap<>();
-            debtorMessageData.put(receipt.getIoMessageData().getIdMessageDebtor(), receipt.getIdEvent());
-            messagesNotified.add(debtorMessageData);
-
-            String payerMessageId = receipt.getIoMessageData().getIdMessagePayer();
-
-            if (payerMessageId != null && !payerMessageId.isEmpty()) {
-                Map<String, String> payerMessageData = new HashMap<>();
-                payerMessageData.put(payerMessageId, receipt.getIdEvent());
-                messagesNotified.add(payerMessageData);
-
-            } else {
-                if (!notifyOnlyDebtor) {
-                    throw new ErrorToNotifyException("Error sending notification to IO user (payer)");
-                }
-            }
-
-            receipt.setStatus(ReceiptStatusType.IO_NOTIFIED);
-        } else {
-            throw new ErrorToNotifyException("Error sending notification to IO user");
-        }
-    }
-
-    private static void notifyMessage(String fiscalCode, String userType) {
-        FiscalCodePayload fiscalCodePayload = new FiscalCodePayload();
-        NewMessage message = new NewMessage();
-        IOClient client = new IOClient();
-
-        fiscalCodePayload.setFiscalCode(fiscalCode);
-        //TODO verify user is an IO user
-        //IO /profiles
-        ApiResponse<LimitedProfile> getProfileResponse;
-        try {
-            getProfileResponse = client.getProfileByPOSTWithHttpInfo(fiscalCodePayload);
-        } catch (ApiException e) {
-            //TODO handle error
-            throw new RuntimeException(e);
-        }
-
-        if (getProfileResponse != null &&
-                getProfileResponse.getData() != null &&
-                getProfileResponse.getData().getSenderAllowed()) {
-            //TODO send notification to users
-            //IO /messages
-            try {
-                ApiResponse<CreatedMessage> sendMessageResponse = client.submitMessageforUserWithFiscalCodeInBodyWithHttpInfo(message);
-                IOMessageData messageData = new IOMessageData();
-                if (sendMessageResponse != null &&
-                        sendMessageResponse.getData() != null) {
-                    if (userType.equals(DEBTOR)) {
-                        messageData.setIdMessageDebtor(sendMessageResponse.getData().getId());
-                    } else {
-                        messageData.setIdMessagePayer(sendMessageResponse.getData().getId());
-                    }
-                }
-
-
-            } catch (ApiException e) {
-                //TODO handle error
-                throw new RuntimeException(e);
-            }
-        } else {
-            //TODO handle not allowed / error response
-        }
     }
 }
--- src/main/java/it/gov/pagopa/receipt/pdf/notifier/exception/ErrorToNotifyException.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/exception/ErrorToNotifyException.java
@@ -1,14 +1,26 @@
 package it.gov.pagopa.receipt.pdf.notifier.exception;
 
-/** Thrown in case IO user can't be notified */
-public class ErrorToNotifyException extends Exception{
+/**
+ * Thrown in case IO user can't be notified
+ */
+public class ErrorToNotifyException extends Exception {
 
     /**
-     * Constructs new exception with provided message and cause
+     * Constructs new exception with provided message
      *
      * @param message Detail message
      */
     public ErrorToNotifyException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructs new exception with provided message and cause
+     *
+     * @param message Detail message
+     * @param cause   Throwable Cause
+     */
+    public ErrorToNotifyException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
