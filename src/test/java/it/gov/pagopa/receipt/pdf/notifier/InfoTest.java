b04fb419d97ff265bc7bb2d23b0e597898114e5d
giomella <gioele.mella.st@everis.nttdata.com>
2023-08-01 12:24:57 +0200

    [PRDP-85] fix info response

--- src/test/java/it/gov/pagopa/receipt/pdf/notifier/InfoTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/notifier/InfoTest.java
@@ -1,26 +1,25 @@
 package it.gov.pagopa.receipt.pdf.notifier;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
-import java.util.Optional;
-import java.util.logging.Logger;
-
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
 import it.gov.pagopa.receipt.pdf.notifier.model.AppInfo;
 import it.gov.pagopa.receipt.pdf.notifier.util.HttpResponseMessageMock;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.HttpRequestMessage;
-import com.microsoft.azure.functions.HttpResponseMessage;
-import com.microsoft.azure.functions.HttpStatus;
 import org.mockito.stubbing.Answer;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 class InfoTest {
@@ -45,40 +44,14 @@ class InfoTest {
         HttpResponseMessage response = sut.run(request, executionContextMock);
 
         // test assertion
+        assertNotNull(response);
         assertEquals(HttpStatus.OK, response.getStatus());
-    }
-
-    @SneakyThrows
-    @Test
-    void getInfoOk() {
-
-        // Mocking service creation
-        Logger logger = Logger.getLogger("example-test-logger");
-        String path = "/META-INF/maven/it.gov.pagopa.receipt.pdf.notifier/pagopa-receipt-pdf-notifier/pom.properties";
-
-        // Execute function
-        AppInfo response = sut.getInfo(logger, path);
-
-        // Checking assertions
-        assertNotNull(response.getName());
-        assertNotNull(response.getVersion());
-        assertNotNull(response.getEnvironment());
-    }
-
-    @SneakyThrows
-    @Test
-    void getInfoKo() {
-
-        // Mocking service creation
-        Logger logger = Logger.getLogger("example-test-logger");
-        String path = "/META-INF/maven/it.gov.pagopa.receipt.pdf.notifier/pagopa-receipt-pdf-notifier/fake";
-
-        // Execute function
-        AppInfo response = sut.getInfo(logger, path);
-
-        // Checking assertions
-        assertNull(response.getName());
-        assertNull(response.getVersion());
-        assertNotNull(response.getEnvironment());
+        assertNotNull(response.getBody());
+        AppInfo responseBody = (AppInfo) response.getBody();
+        assertNotNull(responseBody.getName());
+        assertNotNull(responseBody.getVersion());
+        assertNotNull(responseBody.getEnvironment());
+        assertEquals("pagopa-receipt-pdf-notifier", responseBody.getName());
+        assertEquals("azure-fn", responseBody.getEnvironment());
     }
 }
