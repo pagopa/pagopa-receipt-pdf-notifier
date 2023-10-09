f3f786473ee46b6caaad0987c8fc43903046da5e
giomella <gioele.mella.st@everis.nttdata.com>
2023-10-09 10:13:58 +0200

    updated receipt model and fix step name

--- integration-test/src/step_definitions/common.js
+++ integration-test/src/step_definitions/common.js
@@ -8,7 +8,14 @@ function createReceipt(id) {
 		"eventId": id,
 		"eventData": {
 			"payerFiscalCode": "AAAAAA00A00A000A",
-			"debtorFiscalCode": "AAAAAA00A00A000A"
+			"debtorFiscalCode": "AAAAAA00A00A000A",
+			"amount": "200",
+			"cart": [
+				{
+					"payeeName": "Comune di Milano",
+					"subject": "ACI"
+				}
+			]
 		},
 		"status": "GENERATED",
 		"numRetry": 0,
