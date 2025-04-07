d008f41856dc5ee33601a52d3b8e857f7a65eb75
giomella <giomella@emeal.nttdata.com>
2025-04-07 09:53:12 +0200

    [PIDM-355] updated receipt client with query on io message container

--- src/main/java/it/gov/pagopa/receipt/pdf/notifier/client/ReceiptCosmosClient.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/client/ReceiptCosmosClient.java
@@ -1,10 +1,15 @@
 package it.gov.pagopa.receipt.pdf.notifier.client;
 
 
+import it.gov.pagopa.receipt.pdf.notifier.entity.message.IOMessage;
 import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.Receipt;
+import it.gov.pagopa.receipt.pdf.notifier.exception.IoMessageNotFoundException;
 import it.gov.pagopa.receipt.pdf.notifier.exception.ReceiptNotFoundException;
+import it.gov.pagopa.receipt.pdf.notifier.model.enumeration.UserType;
 
 public interface ReceiptCosmosClient {
 
     Receipt getReceiptDocument(String receiptId) throws ReceiptNotFoundException;
+
+    IOMessage findIOMessageWithEventIdAndUserType(String eventId, UserType userType) throws IoMessageNotFoundException;
 }
