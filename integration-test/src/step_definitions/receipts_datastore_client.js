10149108b0d7da3a44944e7b3fdee2b83874b04d
giomella <giomella@emeal.nttdata.com>
2025-11-28 12:17:35 +0100

    [PAGOPA-3374] updated client to handle cart receipts

--- integration-test/src/step_definitions/receipts_datastore_client.js
+++ integration-test/src/step_definitions/receipts_datastore_client.js
@@ -1,33 +1,26 @@
 const { CosmosClient } = require("@azure/cosmos");
-const {createReceipt, createReceiptForError} = require("./common");
+const { createReceipt, createReceiptForError, createCartReceipt, createCartReceiptForError } = require("./common");
 
-const cosmos_db_conn_string  = process.env.RECEIPTS_COSMOS_CONN_STRING || "";
-const databaseId             = process.env.RECEIPT_COSMOS_DB_NAME; 
-const containerId            = process.env.RECEIPT_COSMOS_DB_CONTAINER_NAME;
+const cosmos_db_conn_string = process.env.RECEIPTS_COSMOS_CONN_STRING || "";
+const databaseId = process.env.RECEIPT_COSMOS_DB_NAME;
+const containerId = process.env.RECEIPT_COSMOS_DB_CONTAINER_NAME;
+const cartContainerId = process.env.CART_RECEIPT_COSMOS_DB_CONTAINER_NAME;
 
 const client = new CosmosClient(cosmos_db_conn_string);
 const container = client.database(databaseId).container(containerId);
+const cartContainer = client.database(databaseId).container(cartContainerId);
 
 async function getDocumentByIdFromReceiptsDatastore(id) {
     return await container.items
-                    .query({
-                        query: "SELECT * from c WHERE c.eventId=@eventId",
-                        parameters: [{ name: "@eventId", value: id }]
-                      })
-                    .fetchNext();
+        .query({
+            query: "SELECT * from c WHERE c.eventId=@eventId",
+            parameters: [{ name: "@eventId", value: id }]
+        })
+        .fetchNext();
 }
 
-async function createDocumentInReceiptsDatastore(id) {  
+async function createDocumentInReceiptsDatastore(id) {
     let event = createReceipt(id);
-    try {
-        return await container.items.create(event);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-async function createErrorDocumentInReceiptsDatastore(id) {
-    let event = createReceiptForError(id);
     try {
         return await container.items.create(event);
     } catch (err) {
@@ -45,6 +38,43 @@ async function deleteDocumentFromReceiptsDatastore(id, partitionKey) {
     }
 }
 
+////////////////////////////////////////////////////
+////////////////        Cart        ////////////////
+////////////////////////////////////////////////////
+
+async function getDocumentByIdFromCartReceiptsDatastore(id) {
+    return await cartContainer.items
+        .query({
+            query: "SELECT * from c WHERE c.eventId=@eventId",
+            parameters: [{ name: "@eventId", value: id }]
+        })
+        .fetchNext();
+}
+
+async function createDocumentInCartReceiptsDatastore(id) {
+    let event = createCartReceipt(id);
+    try {
+        return await cartContainer.items.create(event);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteDocumentFromCartReceiptsDatastore(id, partitionKey) {
+    try {
+        return await cartContainer.item(id, partitionKey).delete();
+    } catch (error) {
+        if (error.code !== 404) {
+            console.log(error)
+        }
+    }
+}
+
 module.exports = {
-    getDocumentByIdFromReceiptsDatastore, createDocumentInReceiptsDatastore, deleteDocumentFromReceiptsDatastore, createErrorDocumentInReceiptsDatastore
+    getDocumentByIdFromReceiptsDatastore,
+    createDocumentInReceiptsDatastore,
+    deleteDocumentFromReceiptsDatastore,
+    getDocumentByIdFromCartReceiptsDatastore,
+    createDocumentInCartReceiptsDatastore,
+    deleteDocumentFromCartReceiptsDatastore
 }
