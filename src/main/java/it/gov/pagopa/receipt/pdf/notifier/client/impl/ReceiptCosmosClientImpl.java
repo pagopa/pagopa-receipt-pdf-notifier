60163923890323b52aacdeaa5a3d793bc9a2b81c
giomella <giomella@emeal.nttdata.com>
2026-06-24 13:02:36 +0200

    [PAGOPA-3625] remove unused query

--- src/main/java/it/gov/pagopa/receipt/pdf/notifier/client/impl/ReceiptCosmosClientImpl.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/notifier/client/impl/ReceiptCosmosClientImpl.java
@@ -9,23 +9,19 @@ import com.azure.cosmos.models.SqlParameter;
 import com.azure.cosmos.models.SqlQuerySpec;
 import it.gov.pagopa.receipt.pdf.notifier.client.ReceiptCosmosClient;
 import it.gov.pagopa.receipt.pdf.notifier.entity.message.IOMessage;
-import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.notifier.exception.IoMessageNotFoundException;
-import it.gov.pagopa.receipt.pdf.notifier.exception.ReceiptNotFoundException;
 import it.gov.pagopa.receipt.pdf.notifier.model.enumeration.UserType;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
- * Client for the CosmosDB database
+ * {@inheritDoc}
  */
 public class ReceiptCosmosClientImpl implements ReceiptCosmosClient {
 
     private static ReceiptCosmosClientImpl instance;
 
     private final String databaseId = System.getenv("COSMOS_RECEIPT_DB_NAME");
-    private final String containerId = System.getenv("COSMOS_RECEIPT_CONTAINER_NAME");
     private final String containerMessageId = System.getenv("COSMOS_RECEIPT_MESSAGE_CONTAINER_NAME");
 
     private final CosmosClient cosmosClient;
@@ -53,37 +49,7 @@ public class ReceiptCosmosClientImpl implements ReceiptCosmosClient {
     }
 
     /**
-     * Retrieve receipt document from CosmosDB database
-     *
-     * @param eventId Biz-event id
-     * @return receipt document
-     * @throws ReceiptNotFoundException in case no receipt has been found with the given idEvent
-     */
-    public Receipt getReceiptDocument(String eventId) throws ReceiptNotFoundException {
-        CosmosDatabase cosmosDatabase = this.cosmosClient.getDatabase(databaseId);
-        CosmosContainer cosmosContainer = cosmosDatabase.getContainer(containerId);
-
-        //Build query
-        SqlQuerySpec querySpec = new SqlQuerySpec(
-                "SELECT * FROM c WHERE c.eventId = @eventId",
-                List.of(new SqlParameter("@eventId", eventId))
-        );
-
-        //Query the container
-        return cosmosContainer
-                .queryItems(querySpec, new CosmosQueryRequestOptions(), Receipt.class)
-                .stream()
-                .findFirst()
-                .orElseThrow(() -> new ReceiptNotFoundException("Document not found in the defined container"));
-    }
-
-    /**
-     * Retrieve io message document from CosmosDB database with the provided event id and user type
-     *
-     * @param eventId  Receipt event id
-     * @param userType Recipient of the IO message
-     * @return io message document
-     * @throws IoMessageNotFoundException in case no receipt has been found with the given eventId and user type
+     * {@inheritDoc}
      */
     @Override
     public IOMessage findIOMessageWithEventIdAndUserType(
--- src/test/java/it/gov/pagopa/receipt/pdf/notifier/client/impl/ReceiptCosmosClientImplTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/notifier/client/impl/ReceiptCosmosClientImplTest.java
@@ -3,13 +3,10 @@ package it.gov.pagopa.receipt.pdf.notifier.client.impl;
 import com.azure.cosmos.CosmosClient;
 import com.azure.cosmos.CosmosContainer;
 import com.azure.cosmos.CosmosDatabase;
-import com.azure.cosmos.models.FeedResponse;
 import com.azure.cosmos.models.SqlQuerySpec;
 import com.azure.cosmos.util.CosmosPagedIterable;
 import it.gov.pagopa.receipt.pdf.notifier.entity.message.IOMessage;
-import it.gov.pagopa.receipt.pdf.notifier.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.notifier.exception.IoMessageNotFoundException;
-import it.gov.pagopa.receipt.pdf.notifier.exception.ReceiptNotFoundException;
 import it.gov.pagopa.receipt.pdf.notifier.model.enumeration.UserType;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,13 +36,7 @@ class ReceiptCosmosClientImplTest {
     @Mock
     private CosmosContainer mockContainer;
     @Mock
-    private CosmosPagedIterable<Receipt> mockIterable;
-    @Mock
     private CosmosPagedIterable<IOMessage> mockIOMessageIterable;
-    @Mock
-    private Iterable<FeedResponse<Receipt>> mockReceiptIterableByPage;
-    @Mock
-    private Stream<Receipt> mockReceiptStream;
     @Mock
     private Stream<IOMessage> mockIOMessageStream;
 
@@ -59,37 +50,6 @@ class ReceiptCosmosClientImplTest {
                 "COSMOS_RECEIPT_KEY", mockKey,
                 "COSMOS_RECEIPT_SERVICE_ENDPOINT", ""
         ).execute(() -> assertThrows(IllegalArgumentException.class, ReceiptCosmosClientImpl::getInstance)
-        );
-    }
-
-    @Test
-    void getReceiptDocumentSuccess() {
-        String receiptId = "a valid receipt id";
-        Receipt receipt = new Receipt();
-        receipt.setId(receiptId);
-
-        when(cosmosClientMock.getDatabase(any())).thenReturn(mockDatabase);
-        when(mockDatabase.getContainer(any())).thenReturn(mockContainer);
-        when(mockContainer.queryItems(any(SqlQuerySpec.class), any(), eq(Receipt.class))).thenReturn(mockIterable);
-        when(mockIterable.stream()).thenReturn(mockReceiptStream);
-        when(mockReceiptStream.findFirst()).thenReturn(Optional.of(receipt));
-
-        Receipt response = assertDoesNotThrow(() -> sut.getReceiptDocument(receiptId));
-
-        assertNotNull(response);
-        assertEquals(receiptId, response.getId());
-    }
-
-    @Test
-    void getReceiptDocumentFailNotFound() {
-        when(cosmosClientMock.getDatabase(any())).thenReturn(mockDatabase);
-        when(mockDatabase.getContainer(any())).thenReturn(mockContainer);
-        when(mockContainer.queryItems(any(SqlQuerySpec.class), any(), eq(Receipt.class))).thenReturn(mockIterable);
-        when(mockIterable.stream()).thenReturn(mockReceiptStream);
-        when(mockReceiptStream.findFirst()).thenReturn(Optional.empty());
-
-        assertThrows(ReceiptNotFoundException.class,
-                () -> sut.getReceiptDocument("an invalid receipt id")
         );
     }
 
@@ -125,5 +85,4 @@ class ReceiptCosmosClientImplTest {
                 () -> sut.findIOMessageWithEventIdAndUserType("eventId", UserType.DEBTOR)
         );
     }
-
 }
